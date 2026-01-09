51bce6885572f37c72b22a10ad9e763587ad16da
TrieuThien2007 <nguyentrieuthien2906@gmail.com>
2026-01-09 15:34:02 -0800

    Add code for welcome message

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,5 +3,6 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! I'm Thien Nguyen");
+        System.out.println("Welcome to the class!");
     }
 }
